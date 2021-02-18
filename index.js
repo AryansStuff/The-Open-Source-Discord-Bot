3bd5edf06cf63d8120b1590543d12dea7188a558
AyuuDubbed <70710798+DiscordBotCreator-png@users.noreply.github.com>
2021-02-18 12:45:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,41 +7,8 @@ const dotenv = require('dotenv');
 let prefixChange = false;
 dotenv.config();
 // Logs ready to console when bot turns on
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-client.on('ready', async () => {
-	console.log('Im in');
-	console.log(client.user.username);
-
-	let i = 0;
-
-	setInterval(function() {
-		const activities = [`${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!`];
-
-		client.user.setActivity(activities[i], { type: 'WATCHING' });
-
-		i = (i + 1) % activities.length;
-	}, 5 * 1000);
-
-	client.on('guildCreate', async guild => {
-		client.gData.set(guild.id, {
-			_id: guild.id,
-		});
-	});
-	client.on('guildDelete', async guild => {
-		client.gData.delete(guild.id);
-	});
-
-	await sleep(100);
-
-	client.guilds.cache.forEach(guild => {
-		if (!client.gData(guild.id)) client.emit('guildCreate', (guild));
-	});
-	client.dat(data => {
-		if (!client.guilds.cache.has(data._id)) client.emit('guildDelete', (data));
-	});
-
-	console.log(client.gData);
+client.on('ready', () => {
+	console.log('Im Ready');
 });
 let prefix = '?';
 client.on('message', message => {
