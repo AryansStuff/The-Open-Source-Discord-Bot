31f9a68e855a7d66ffb7ccd5dd4ea96ab07b6ffe
AyuuDubbed <70710798+DiscordBotCreator-png@users.noreply.github.com>
2021-02-20 20:19:29 -0600

    Index.js concatenation error fixed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable no-const-assign */
 /* eslint-disable no-unused-vars */
 // Vars, Imports, And Configs
@@ -5,6 +7,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const dotenv = require('dotenv');
 let prefixChange = false;
+let needHelp = false;
+let helpWith = 'Nothing';
 dotenv.config();
 // Logs ready to console when bot turns on
 client.on('ready', () => {
@@ -20,17 +24,36 @@ client.on('message', message => {
 		prefix = message.content;
 		console.log(prefix);
 		prefixChange = false;
-		message.channel.send('The prefix was changed to', prefix);
+		message.channel.send(`The prefix was changed to ${prefix}`);
+		message.channel.send('THIS PREFIX WONT SAVE AFTER THE NEXT RESTART, THIS WILL BE FIXED SO PLEASE DONT SPAM ME WITH MY PREFIX ISNT SAVED');
+	}
+	if (needHelp == true) {
+        if(message.author.bot) {
+			return;
+		}
+		helpWith = message.content;
+		console.log(helpWith);
+        if (helpWith == 'name') {
+             // eslint-disable-next-line no-empty
+            message.channel.send(process.env.NameCMD);
+        }
+       
+		needHelp = false;
+		
 	}
 	// name method call for bot
 	if (message.content == prefix + 'name' && message.author != message.author.bot) {
-		message.author.send('hello,', message.author.username, ' my name is MonkeOS Support Bot and the server you are in is called ', message.guild.name);
-		console.log('hello,', message.author.username, 'my name is MonkeOS Support Bot and the server you are in is called ', message.guild.name);
+		message.author.send(`hello, ${message.author.username}, my name is MonkeOS Support Bot and the server you are in is called ${message.guild.name}`);
+		console.log(`hello, ${message.author.username}, my name is MonkeOS Support Bot and the server you are in is called ${message.guild.name}`);
 	}
 	// changeprefix method call for bot
 	if (message.content == prefix + 'changePrefix' && message.author != message.author.bot) {
 		message.channel.send('What would you like me to change it to?');
 		prefixChange = true;
+	}
+	if (message.content == prefix + 'help') {
+		message.channel.send('What command do you want to know about?');
+		needHelp = true;
 	}
 	// 'Uncomment these lines if you want to make sure your bot is getting input.';
 	// const x = message.content;
