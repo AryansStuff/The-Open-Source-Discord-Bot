e6d0ba11ecbb48be3fe7efab32e6347d02c9be76
AyuuDubbed <70710798+DiscordBotCreator-png@users.noreply.github.com>
2021-03-24 08:48:16 -0500

    Index.js Template errors fixed

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const dotenv = require('dotenv');
 let prefixChange = false;
-let needHelp = false;
-let helpWith = 'Nothing';
 dotenv.config();
 // Logs ready to console when bot turns on
 client.on('ready', () => {
@@ -27,20 +25,6 @@ client.on('message', message => {
 		message.channel.send(`The prefix was changed to ${prefix}`);
 		message.channel.send('THIS PREFIX WONT SAVE AFTER THE NEXT RESTART, THIS WILL BE FIXED SO PLEASE DONT SPAM ME WITH MY PREFIX ISNT SAVED');
 	}
-	if (needHelp == true) {
-        if(message.author.bot) {
-			return;
-		}
-		helpWith = message.content;
-		console.log(helpWith);
-        if (helpWith == 'name') {
-             // eslint-disable-next-line no-empty
-            message.channel.send(process.env.NameCMD);
-        }
-       
-		needHelp = false;
-		
-	}
 	// name method call for bot
 	if (message.content == prefix + 'name' && message.author != message.author.bot) {
 		message.author.send(`hello, ${message.author.username}, my name is MonkeOS Support Bot and the server you are in is called ${message.guild.name}`);
@@ -50,10 +34,6 @@ client.on('message', message => {
 	if (message.content == prefix + 'changePrefix' && message.author != message.author.bot) {
 		message.channel.send('What would you like me to change it to?');
 		prefixChange = true;
-	}
-	if (message.content == prefix + 'help') {
-		message.channel.send('What command do you want to know about?');
-		needHelp = true;
 	}
 	// 'Uncomment these lines if you want to make sure your bot is getting input.';
 	// const x = message.content;
